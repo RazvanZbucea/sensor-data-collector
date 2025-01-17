f390fa12a8ea4efad17a3451dd329af4443bcdcd
Razvan Zbucea <razvan.zbucea@globallogic.com>
2025-01-17 21:23:05 +0200

    Initial commit

---
+++ src/main/java/com/example/sensor_data_collector/SensorDataCollectorApplication.java
@@ -0,0 +1,13 @@
+package com.example.sensor_data_collector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorDataCollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensorDataCollectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sensor_data_collector/SensorDataCollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sensor_data_collector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorDataCollectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
